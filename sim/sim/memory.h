00bae0bdd64e99a93eb4bf055173b833809f219c
danielashabat <danielashabat@gmail.com>
2020-12-06 21:06:19 +0200

    ..

--- sim/sim/memory.h
+++ sim/sim/memory.h
@@ -4,6 +4,10 @@
 /*this module includes all the functions and structs that belong to the main memory and the cache*/
 
 
+/*********************MACROS*****************/
+#define MainMemorySize 1048576//equal to 2^20
+
+
 /*********************STRUCTS*****************/
 
 typedef struct Cache {
